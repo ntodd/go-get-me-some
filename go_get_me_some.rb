25b22ea0293d55335f5b9698c5dd99596fe79395
Jonathan Crossman <jonathan@megatron.local>
2009-02-06 12:00:52 -0500

    finding the actual image url

--- go_get_me_some.rb
+++ go_get_me_some.rb
@@ -12,5 +12,9 @@ end
 get '/:topic' do  
   doc = open("http://images.google.com/images?um=1&hl=en&client=safari&rls=en-us&btnG=Search+Images&ei=ubeLSbSgGqTUMbzxzZAL&gbv=1&ei=lLqLSYWsFaX6NJrzyIkL&q=#{params[:topic]}") { |f| Hpricot(f) }
   @image = (doc/"table/tr/td/a/img").first
+  
+  # anchor = (doc/"div#ImgCont/table/tr/td/a").first
+  # @address = anchor.to_s.match(/imgurl=(http:\/\/[^&]+)/)[1]
+  
   haml :view
 end
