54a312c4aed8b588838c6ecc89d6c629f895c0e7
Nate Todd <nate@natetodd.com>
2009-02-06 12:37:43 -0500

    escaped url so it can accept spaces.  commented some things.

--- go_get_me_some.rb
+++ go_get_me_some.rb
@@ -9,10 +9,15 @@ get '/' do
   haml :index
 end
 
-get '/:topic' do  
-  doc = open("http://images.google.com/images?um=1&hl=en&client=safari&rls=en-us&btnG=Search+Images&ei=ubeLSbSgGqTUMbzxzZAL&gbv=1&ei=lLqLSYWsFaX6NJrzyIkL&q=#{params[:topic]}") { |f| Hpricot(f) }
+get '/:topic' do
+  doc = open("http://images.google.com/images?um=1&hl=en&client=safari&rls=en-us&btnG=Search+Images&ei=ubeLSbSgGqTUMbzxzZAL&gbv=1&ei=lLqLSYWsFaX6NJrzyIkL&q=#{CGI.escape(params[:topic])}") { |f| Hpricot(f) }
+
+  #grab full google img element for backup display if hotlinking fails
   @google_image = (doc/"table/tr/td/a/img").first  
+
+  # fetch hotlinking image source
   anchor = (doc/"div#ImgCont/table/tr/td/a").first
   @remote_image_src = anchor.to_s.match(/imgurl=(http:\/\/[^&]+)/)[1] unless anchor.nil?
+
   haml :view
 end
