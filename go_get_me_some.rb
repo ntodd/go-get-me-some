7206dd0bb9347f98d7cdf89e8ae3dc8c62fb75a1
Nate Todd <nate@natetodd.com>
2009-02-05 23:41:38 -0500

    initial import

---
+++ go_get_me_some.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'hpricot'
+require 'open-uri'
+
+get '/' do
+  haml :index
+end
+
+
+get '/:topic' do
+  
+  doc = open("http://images.google.com/images?um=1&hl=en&client=safari&rls=en-us&btnG=Search+Images&ei=ubeLSbSgGqTUMbzxzZAL&gbv=1&ei=lLqLSYWsFaX6NJrzyIkL&q=#{params[:topic]}") { |f| Hpricot(f) }
+  
+  @image = (doc/"table/tr/td/a/img").first
+  
+  haml :view
+end
