032138be3b68e33878dfcaa661624f92492d2c07
Jonathan Crossman <jonathan@megatron.local>
2009-02-09 22:52:50 -0500

    forgot about the index page... making the js fail nicely

--- public/go_get_me_some.js
+++ public/go_get_me_some.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 	var img = document.getElementsByTagName('img')[0];
 	
-	if (img.naturalHeight + img.naturalWidth == 0)
+	if (img && img.naturalHeight + img.naturalWidth == 0)
 		img.src = thumbnail;
 }
